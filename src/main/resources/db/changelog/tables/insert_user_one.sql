8ebe01aba1bbe0a1543f564c1caf4a1731d1bda0
Igor Egorov <igor73@list.ru>
2023-03-16 12:04:31 +0300

    С исправлениями

--- src/main/resources/db/changelog/tables/insert_user_one.sql
+++ src/main/resources/db/changelog/tables/insert_user_one.sql
@@ -1,2 +1,2 @@
 insert into users (username, password)
-values ('user@one.user', 'kvjrkjglrkghl;rtg;le[4');
+values ('user@one.user', '$2a$10$h7QmnlSEXLF2eQS4lIlRX.FXGZ0U8tpcLYi8IPkgLG/sqQiDA/HlG');
--- src/main/resources/db/changelog/tables/insert_user_two.sql
+++ src/main/resources/db/changelog/tables/insert_user_two.sql
@@ -1,2 +1,2 @@
 insert into users (username, password)
-values ('user@two.user', 'fhtyiuouyujtrytu7i76666g6');
+values ('user@two.user', '$2a$10$kNOJuKpknuUJPnOinunYDOEyJq2v3qXvyiaCMoS2CKOobT8Mfi8Da');
